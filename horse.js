10aa8dd362690efe8eb5ca4d2c9cc20c88567277
Vothitonhu <tonhuiqsc@gmail.com>
2020-03-27 09:23:00 +0700

    Add method eat

--- horse.js
+++ horse.js
@@ -7,4 +7,7 @@ Horse.prototype.sleep = function () {
 Horse.prototype.run = function () {
     console.log("Running...")
 }
+Horse.prototype.eat = function () {
+    console.log('Horse eat grass')
+}
 module.exports = Horse
